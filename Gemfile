41f6dd6287f54b5ca60ed263d5445f1231ff937c
William <william@getbirdly.com>
2017-09-13 20:09:06 +0200

    success!

--- Gemfile
+++ Gemfile
@@ -4,4 +4,3 @@ source 'https://rubygems.org'
 gem 'rspec'
 gem "rack-test", require: "rack/test"
 gem 'sinatra', '2.0.0.beta2'
-
--- app/api.rb
+++ app/api.rb
@@ -1,10 +1,42 @@
 require 'bundler/setup'
 require 'sinatra/base'
+require 'json'
 
-class Api < Sinatra::Base
-
-	get '/hello' do
-		'hello world!'
+class JSONparser
+	def initialize(filename: 'data.json')
+		@data = JSON.parse(IO.read(filename))
 	end
 
+	def movies
+		@data['movies']
+	end
+
+	def directors
+		@data['directors']
+	end
+end
+
+class Api < Sinatra::Base
+	get '/hello' do
+		'Hello world!'
+	end
+
+	get '/movies' do
+		content_type :json
+		data.movies.to_json
+	end
+
+	get '/movies/:id' do
+		content_type :json
+		movies = data.movies
+		movies.select{|movie| movie['id'] == params[:id].to_i}.first.to_json
+	end
+
+
+	private
+
+	def data
+		@data ||= JSONparser.new(filename: 'data.json')
+		@data
+	end
 end
